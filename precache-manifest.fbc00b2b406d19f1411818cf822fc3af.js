92c0fdb1e1b7afc2546f26931e651662bfa25fe3
charles <okekeemeka20@gmail.com>
2019-10-14 21:55:21 -0400

    Updates

---
+++ precache-manifest.fbc00b2b406d19f1411818cf822fc3af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0fe93a12bd5199a5d0b25d18eb569d",
+    "url": "/Burgerapp/index.html"
+  },
+  {
+    "revision": "37820fa6fe705e4a5859",
+    "url": "/Burgerapp/static/css/main.4e164196.chunk.css"
+  },
+  {
+    "revision": "42972504bb1af0d2a17c",
+    "url": "/Burgerapp/static/js/2.eb6bcef6.chunk.js"
+  },
+  {
+    "revision": "37820fa6fe705e4a5859",
+    "url": "/Burgerapp/static/js/main.96c8eb71.chunk.js"
+  },
+  {
+    "revision": "9f4c50818f50a10ab89b",
+    "url": "/Burgerapp/static/js/runtime~main.5b2d5e47.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burgerapp/static/media/burger-logo.b8503d26.png"
+  }
+]);
